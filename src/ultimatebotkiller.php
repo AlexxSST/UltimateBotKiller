71cc73323fb0e5407d2f500a76d7d3600ce16130
Alemalakra <alemalakra@gmail.com>
2018-01-19 16:49:30 -0600

    Update ultimatebotkiller.php

--- src/ultimatebotkiller.php
+++ src/ultimatebotkiller.php
@@ -6,6 +6,24 @@
  *  @version 1.0
  */
 
+// NGINX and UnSupported Versions.
+
+if (!function_exists('getallheaders')) 
+{ 
+    function getallheaders() 
+    { 
+       $headers = array (); 
+       foreach ($_SERVER as $name => $value) 
+       { 
+           if (substr($name, 0, 5) == 'HTTP_') 
+           { 
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+           } 
+       } 
+       return $headers; 
+    } 
+} 
+
 namespace Alemalakra\UltimateBotKiller;
 require('Packer.php');
 
