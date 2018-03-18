d58015494e97f470a3d35334402c2a3fd2f784b5
Alemalakra ✓ <alemalakra@gmail.com>
2018-03-18 03:23:54 -0600

    3.0

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
  *  UltimateBotKiller - PHP Library For Block 99.99% of Malicious Bots.
  *
  *  @author Alemalakra
- *  @version 2.0
+ *  @version 3.0
  */
 
 // Load class, and load it.
@@ -12,27 +12,32 @@ $UBK = new Alemalakra\UltimateBotKiller\UBK();
 
 // Check if post is set.
 if ($UBK->validateForm()) {
-	echo 'Post validated without errors!';
+	if ($UBK->getValueInput("somepostinput")) {
+		echo 'Post validated without errors!';
+	}
 }
 
 // Encrypt JavaScript Code.
-for ($i=0; $i < rand(3,10); $i++) {
-	if (isset($_s)) {
-		$tmp = new Packer($_s, 'Normal', true, false, true);
-		$tmp = $tmp->pack();
-		$_s = $tmp;
-		unset($tmp);
-	} else {
-		$_s = new Packer($UBK->getCode(), 'Normal', true, false, true);
-		$_s = $_s->pack();
-		$_s = $_j->ubk($_s);
-	}
+for ($i = 0; $i < rand(3, 10); $i++) {
+    if (isset($_s)) {
+        $tmp = new Packer($_s, 'Normal', true, false, true);
+        $tmp = $tmp->pack();
+        $_s = $tmp;
+        unset($tmp);
+    } else {
+        $_s = new Packer($UBK->getCode(), 'Normal', true, false, true);
+        $_s = $_s->pack();
+        $_s = $_j->ubk($_s);
+    }
 }
+// After check if the form was sent.
+$FormInput1 = $UBK->getNameInput('somepostinput');
 ?>
 <center>
 	<form method="post">
-		Sample Input: <input type="text" name="someinput" value="Any form input" />
-		<?php echo $UBK->getInput($_s); ?>
+		Sample Input:
+		<input type="text" name="<?php echo $FormInput1; ?>" value="Any form input" />
+		<?php echo $UBK->getInputBotKiller($_s); ?>
 		<button type="submit">Submit Form POST</button>
 	</form>
 </center>
